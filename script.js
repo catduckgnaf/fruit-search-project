c524e137e37b3c1d1f0d45489e65f39ec75da608
cellardoor452 <github@catduck.org>
2023-08-18 12:57:29 -0400

    Version 0.1

--- script.js
+++ script.js
@@ -5,24 +5,130 @@ const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackb
 
 function search(str) {
 	let results = [];
-
-	// TODO
-
+  
+	for (let i = 0; i < fruit.length; i++) {
+	  const fruitName = fruit[i];
+	  if (fruitName.toLowerCase().includes(str.toLowerCase())) {
+		results.push(fruitName);
+	  }
+	}
+  
 	return results;
-}
+  }
+  
+  function searchHandler(e) {
+	const inputVal = e.target.value;
+	if (inputVal.length === 0) {
+	  suggestions.classList.remove('has-suggestions');
+	} else {
+	  const results = search(inputVal);
+	  showSuggestions(results);
+	}
+  }
+  
+  function showSuggestions(results) {
+	suggestions.innerHTML = '';
+	results.forEach(result => {
+	  const li = document.createElement('li');
+	  li.textContent = result;
+	  suggestions.appendChild(li);
+	});
+	suggestions.classList.add('has-suggestions');
+  }
+  
+  let highlightedIndex = -1;
 
-function searchHandler(e) {
-	// TODO
-}
+  function highlightSuggestion(index) {
+	removeHighlight();
+	if (index >= 0 && index < suggestions.children.length) {
+	  suggestions.children[index].classList.add('highlighted');
+	  highlightedIndex = index;
+	}
+  }
+  
+  function removeHighlight() {
+	if (highlightedIndex !== -1) {
+	  suggestions.children[highlightedIndex].classList.remove('highlighted');
+	  highlightedIndex = -1;
+	}
+  }
+  
+  function useSuggestion(e) {
+	if (e.target.tagName === 'LI') {
+	  input.value = e.target.textContent;
+	  suggestions.innerHTML = '';
+	  suggestions.classList.remove('has-suggestions');
+	  input.placeholder = input.value; // Update the placeholder with the selected suggestion
+	}
+  }
+  
+  input.addEventListener('input', searchHandler);
+  suggestions.addEventListener('click', useSuggestion);
+  
+  input.addEventListener('keydown', e => {
+	if (e.key === 'ArrowDown') {
+	  e.preventDefault();
+	  if (highlightedIndex < suggestions.children.length - 1) {
+		highlightSuggestion(highlightedIndex + 1);
+	  }
+	} else if (e.key === 'ArrowUp') {
+	  e.preventDefault();
+	  if (highlightedIndex > 0) {
+		highlightSuggestion(highlightedIndex - 1);
+	  }
+	} else if (e.key === 'Enter') {
+	  if (highlightedIndex !== -1) {
+		e.preventDefault();
+		input.value = suggestions.children[highlightedIndex].textContent;
+		suggestions.innerHTML = '';
+		suggestions.classList.remove('has-suggestions');
+		input.placeholder = input.value; // Update the placeholder with the selected suggestion
+	  }
+	}
+  });
+  
 
-function showSuggestions(results, inputVal) {
-
-	// TODO
-}
-
-function useSuggestion(e) {
-	// TODO
-}
-
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+  function useSuggestion(e) {
+	if (e.target.tagName === 'LI') {
+	  input.value = e.target.textContent;
+	  suggestions.innerHTML = '';
+	  suggestions.classList.remove('has-suggestions');
+	  input.placeholder = input.value; 
+	}
+  }
+  
+  input.addEventListener('input', searchHandler);
+  suggestions.addEventListener('click', useSuggestion);
+  
+  input.addEventListener('focus', () => {
+	if (suggestions.children.length > 0) {
+	  suggestions.classList.add('has-suggestions');
+	}
+  });
+  
+  input.addEventListener('blur', () => {
+	suggestions.classList.remove('has-suggestions');
+  });
+  
+  // Add random color on hover
+  suggestions.addEventListener('mouseover', (e) => {
+	if (e.target.tagName === 'LI') {
+	  const randomColor = generateRandomColor();
+	  e.target.style.backgroundColor = randomColor;
+	}
+  });
+  
+  suggestions.addEventListener('mouseout', (e) => {
+	if (e.target.tagName === 'LI') {
+	  e.target.style.backgroundColor = '';
+	}
+  });
+  
+  function generateRandomColor() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+  }
